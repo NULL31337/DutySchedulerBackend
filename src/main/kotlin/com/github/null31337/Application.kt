cf61d3889c44d17dc0b1f2cfbcbf834499cc922b
nagibin02@gmail.com <15062128Aprel>
2022-11-29 23:35:02 +0300

    Initial commit

---
+++ src/main/kotlin/com/github/null31337/Application.kt
@@ -0,0 +1,18 @@
+package com.github.null31337
+
+import io.ktor.server.application.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import com.github.null31337.plugins.*
+
+fun main() {
+  embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
+    .start(wait = true)
+}
+
+fun Application.module() {
+  configureSecurity()
+  configureSerialization()
+  configureTemplating()
+  configureRouting()
+}
